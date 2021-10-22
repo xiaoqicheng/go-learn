f59bec107f0cfe4089d67016682a8332e44a0af9
xiaoqicheng <1261964818@qq.com>
2021-10-22 17:59:29 +0800

    add waitGroup

--- biz/waitGroup/waitGroup.go
+++ biz/waitGroup/waitGroup.go
@@ -1,1 +1,47 @@
 package waitGroup
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+//直接上例子
+
+//线程安全计数器
+type Counter struct {
+	mu sync.Mutex
+	count uint64
+}
+
+func (c *Counter) Incr() {
+	c.mu.Lock()
+	c.count++
+	c.mu.Unlock()
+}
+
+func (c *Counter) Count() uint64 {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.count
+}
+
+func worker(c *Counter, wg *sync.WaitGroup)  {
+	defer wg.Done()
+	time.Sleep(time.Second)
+	c.Incr()
+}
+
+func Example()  {
+	var counter Counter
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go worker(&counter, &wg)
+	}
+
+	wg.Wait()
+	fmt.Println(counter.count)
+}
